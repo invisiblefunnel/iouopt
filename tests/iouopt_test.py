b1ff337869310a27352be9ace77663d3f310ec04
Daniel Whalen <daniel.r.whalen@gmail.com>
2024-08-08 09:18:08 -0700

    Initial commit

---
+++ tests/iouopt_test.py
@@ -0,0 +1,118 @@
+import random
+import unittest
+from collections import defaultdict
+from dataclasses import dataclass
+from typing import Dict, List, Set, Tuple
+
+from iouopt import Journal
+
+
+@dataclass
+class IOU:
+    borrower: int
+    lender: int
+    amount: int
+
+
+class JournalTest(unittest.TestCase):
+    def test_no_ious(self):
+        j = Journal[str]()
+        self.assertListEqual([], list(j.simplify()))
+
+    def test_same_party(self):
+        j = Journal[str]()
+        j.append("A", "A", 1)
+        self.assertListEqual([], list(j.simplify()))
+
+    def test_amount_zero(self):
+        j = Journal[str]()
+        j.append("A", "B", 0)
+        self.assertListEqual([], list(j.simplify()))
+
+    def test_net_zero(self):
+        j = Journal[str]()
+        j.append("A", "B", 1)
+        j.append("B", "A", 1)
+        self.assertListEqual([], list(j.simplify()))
+
+    def test_negative_amount(self):
+        j = Journal[str]()
+        with self.assertRaises(AssertionError) as cm:
+            j.append("A", "B", -1)
+        self.assertEqual("amount < 0", str(cm.exception))
+
+    def test_simplify(self):
+        for _ in range(10_000):
+            inputs = generate_ious(
+                num_ious=random.randint(1, 200),
+                num_parties=random.randint(2, 20),
+                max_amount=1000,
+            )
+
+            for strict in (True, False):
+                j = Journal[int](strict)
+                for iou in inputs:
+                    j.append(iou.borrower, iou.lender, iou.amount)
+
+                outputs = []
+                for borrower, lender, amount in j.simplify():
+                    outputs.append(IOU(borrower, lender, amount))
+
+                self.check_settled(inputs, outputs, strict)
+
+    def check_settled(self, inputs: List[IOU], outputs: List[IOU], strict: bool):
+        # Calculate input/expected values
+        input_flow: Dict[int, int] = defaultdict(int)
+        input_pairs: Set[Tuple[int, int]] = set()
+        for iou in inputs:
+            input_flow[iou.lender] += iou.amount
+            input_flow[iou.borrower] -= iou.amount
+            input_pairs.add((iou.borrower, iou.lender))
+
+        # Calculate output/actual values
+        output_flow: Dict[int, int] = defaultdict(int)
+        output_pairs: Set[Tuple[int, int]] = set()
+        output_borrowers: Set[int] = set()
+        output_lenders: Set[int] = set()
+        for iou in outputs:
+            output_flow[iou.lender] += iou.amount
+            output_flow[iou.borrower] -= iou.amount
+            output_pairs.add((iou.borrower, iou.lender))
+            output_borrowers.add(iou.borrower)
+            output_lenders.add(iou.lender)
+
+        # Verify IOUs are settled correctly
+        for party in input_flow.keys():
+            self.assertEqual(input_flow[party], output_flow[party])
+        for party in output_flow.keys():
+            self.assertEqual(input_flow[party], output_flow[party])
+
+        if strict:
+            # Verify that parties transacted in the original set of IOUs
+            for output_pair in output_pairs:
+                self.assertIn(output_pair, input_pairs)
+        else:
+            # Verify that parties are either payer or payee, not both, in the solution
+            self.assertEqual(
+                len(output_borrowers) + len(output_lenders),
+                len(output_borrowers | output_lenders),
+            )
+
+
+def generate_ious(num_ious: int, num_parties: int, max_amount: int) -> List[IOU]:
+    assert num_parties > 1
+    parties = list(range(num_parties))
+
+    groups: List[List[int]] = []
+    while sum(len(g) for g in groups) < num_ious:
+        group = random.sample(parties, random.randint(2, num_parties))
+        groups.append(group)
+
+    result: List[IOU] = []
+    for group in groups:
+        lender = random.choice(group)
+        for borrower in group:
+            amount = random.randint(1, max_amount)
+            result.append(IOU(borrower, lender, amount))
+
+    return result
