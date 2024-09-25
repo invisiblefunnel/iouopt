2b89518c2ada4849c62e1c866a9d65071413f8c7
Daniel Whalen <daniel.r.whalen@gmail.com>
2024-09-25 13:49:36 -0700

    Switch to ortools for min-cost flow solver
    - NetworkX's network simplex implementation is buggy (7562).
    - Remove strict mode for now. Did not immediately work with SimpleMinCostFlow.
    - Run more test cases.

--- iouopt/journal.py
+++ iouopt/journal.py
@@ -1,41 +1,31 @@
 from collections import defaultdict
-from itertools import permutations
-from typing import Dict, Generic, Hashable, Iterator, Set, Tuple, TypeVar
+from typing import Dict, Generic, Hashable, Iterator, List, Tuple, TypeVar
 
-import networkx as nx  # type: ignore
+import numpy as np
 
-T = TypeVar("T", bound=Hashable)
+from iouopt.flow import min_cost_flow
+
+N = TypeVar("N", bound=Hashable)
 
 
-class Journal(Generic[T]):
-    def __init__(self, strict: bool = False):
-        self.nodes: Dict[T, int] = defaultdict(int)
-        self.edges: Set[Tuple[T, T]] = set()
-        self.strict: bool = strict
+class Journal(Generic[N]):
+    def __init__(self):
+        self.nodes: Dict[N, int] = defaultdict(int)
 
-    def append(self, borrower: T, lender: T, amount: int):
+    def append(self, borrower: N, lender: N, amount: int):
         assert amount >= 0, "amount < 0"
         self.nodes[lender] -= amount
         self.nodes[borrower] += amount
-        if self.strict and lender != borrower:
-            self.edges.add((lender, borrower))
 
-    def simplify(self) -> Iterator[Tuple[T, T, int]]:
-        if len(self.nodes) > 0:
-            # Build demand graph
-            G = nx.DiGraph()
-            # Add nodes with `demand` attribute for network simplex
-            for u, demand in self.nodes.items():
-                G.add_node(u, demand=demand)
-            # Add observed edges if strict, else construct a complete graph.
-            for u, v in self.edges if self.strict else permutations(self.nodes, 2):
-                # Set equal edge weights != 0 as a workaround for this bug:
-                # https://github.com/networkx/networkx/issues/7562
-                G.add_edge(u, v, weight=1)
-            # Let networkx do the heavy lifting
-            receivables: Dict[T, Dict[T, int]] = nx.network_simplex(G)[1]
-            # Extract relevant (non-zero) edges
-            for lender, borrowers in receivables.items():
-                for borrower, amount in borrowers.items():
-                    if amount != 0:
-                        yield borrower, lender, amount
+    def simplify(self) -> Iterator[Tuple[N, N, int]]:
+        if len(self.nodes) < 2:
+            return
+
+        nodes: List[N] = []
+        demand = np.zeros(len(self.nodes), dtype=np.int64)
+        for i, (node, d) in enumerate(self.nodes.items()):
+            nodes.append(node)
+            demand[i] = d
+
+        for u, v, amount in min_cost_flow(demand):
+            yield nodes[u], nodes[v], amount
